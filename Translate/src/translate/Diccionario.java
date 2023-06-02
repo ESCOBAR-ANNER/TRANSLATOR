016293dfe3b43e8dfb90455d3e15b9d6b0b5a274
ESCOBAR-ANNER <aescobarc13@miumg.edu.gt>
2023-06-02 17:49:01 -0600

    Subiendo proyectoo

---
+++ Translate/src/translate/Diccionario.java
@@ -0,0 +1,9 @@
+package translate;
+
+import java.util.HashMap;
+
+class Diccionario {
+
+    HashMap Diccionario = new HashMap();
+
+}
---
+++ Translate/src/translate/Translate.java
@@ -0,0 +1,47 @@
+
+package translate;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+     
+public class Translate {
+    
+
+  public static void main(String[] args) throws Exception  {
+String ruta1 = "C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/Translate/src/translate/Lexer.flex";
+        String ruta2 = "C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/Translate/src/translate/LexerCup.flex";
+        String[] rutaS = {"-parser", "Sintax", "C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/Translate/src/translate/Sintax.cup"};
+        generar(ruta1, ruta2, rutaS);
+    
+}
+
+    public static void generar(String ruta1, String ruta2, String[] rutaS) throws IOException, Exception{
+        File archivo;
+        archivo = new File(ruta1);
+        JFlex.Main.generate(archivo);
+        archivo = new File(ruta2);
+        JFlex.Main.generate(archivo);
+        java_cup.Main.main(rutaS);
+        
+        Path rutaSym = Paths.get("C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/Translate/src/translate/sym.java");
+        if (Files.exists(rutaSym)) {
+            Files.delete(rutaSym);
+        }
+        Files.move(
+                Paths.get("C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/sym.java"), 
+                Paths.get("C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/Translate/src/translate/sym.java")
+        );
+        Path rutaSin = Paths.get("C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/Translate/src/translate/Sintax.java");
+        if (Files.exists(rutaSin)) {
+            Files.delete(rutaSin);
+        }
+        Files.move(
+                Paths.get("C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/Sintax.java"), 
+                Paths.get("C:/Users/CURACAO/Desktop/Compiladores Proyect/Translate/Translate/src/translate/Sintax.java")
+        ); 
+    }
+}
